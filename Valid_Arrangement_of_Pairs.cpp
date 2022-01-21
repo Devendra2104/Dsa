0f0209454d4dc118fe6ba0ad1280ac3a89b7569c
Devendra2104 <ds13545@gmail.com>
2022-01-21 17:35:39 +0530

    Idea Eulrean Circuit

--- Valid_Arrangement_of_Pairs.cpp
+++ Valid_Arrangement_of_Pairs.cpp
@@ -31,6 +31,32 @@ template <typename T>
 void print(T&& t) {cout << t << '\n';}
 template <typename T, typename... Args>
 void print(T&& t, Args&&... args){ cout << t << " "; print(forward<Args>(args)...); }
+// input
+template<typename T>  istream& operator>>(istream& is,  vector<T>& v){for (auto& i : v) is >> i;        return is;}
+template<typename T>  ostream& operator<<(ostream& os,  vector<T>& v){for (auto& i : v) os << i << ' '; return os;}
+//debug template
+void __print(int x) {cerr << x;} void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;} void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+//Print Template.
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second);
+    cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""),
+                __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
 
 class Solution {
 public:
@@ -75,6 +101,7 @@ int main(){
         cin>>x>>y;
         a[i] = {x,y};
     }
+    debug(a);
     vector<vector<int>> ans = nw.validArrangement(a);
     print(ans);
 }
